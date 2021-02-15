cbc8bd33467d4c7dd61f2e629d3ad18e4df57b50
dio19 <dionisio.hernandez@sirena.app>
2021-02-14 21:26:02 -0300

    add styles variables and fix breadcrumbs

--- src/components/tools/Breadcrumb.js
+++ src/components/tools/Breadcrumb.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import '../../styles/Breadcrumb.scss';
+import React from "react";
+import '../../styles/Breadcrumb.scss'
 
-export default function Breadcrumb(props){
-    return <ul className={'breadcrumb-container'}>
-        {props.categories ? props.categories.map((category, idx) =>
-            <li className={"breadcrumb"} key={idx}>{category}
-                {idx !== props.categories.length - 1 ? <i/> : null}
-            </li>)
-            : null}
-    </ul>
-}
+export default function Breadcrumb({ categories }) {
+    let i = 0
+    return (
+        <div id='box-category-list' className="text-align-center-phone">
+            {
+                categories ? categories.map(category => {
+                    let htmlReturn = <span key={category+1} id="normal-category">{category} > </span>
+                    if (i === categories.length - 1) {
+                        htmlReturn = <span key={category+1} id="selected-category">{category} </span>
+                    }
+                    i++;
+                    return htmlReturn
+                }) : <span id="normal-category">No existe categoria para este resultado de busqueda</span>
+            }
+        </div>
+    )
+};
